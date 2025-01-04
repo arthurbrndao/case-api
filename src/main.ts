e27f53ddc84029247c28793063ca939f92cb0e83
Arthur Brandao <arthurbrdantas@gmail.com>
2025-01-04 11:06:30 -0300

    feat: set default port to 3333

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.API_PORT ?? 80;
+  const PORT = process.env.API_PORT ?? 3333;
 
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
+
   console.log(`app running in port ${PORT}`);
 }
 bootstrap();
