9757cb41d7769ea74e7112e8d12139ca44d8eed8
Arthur Brandao <arthurbrdantas@gmail.com>
2025-01-05 16:24:10 -0300

    feat: get from initial data endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getInitialUsers(): string {
+  getInitialUsers(): object {
     return this.appService.getInitialUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+
+interface IInitialUsers {
+  status: number;
+  body: string;
+}
 
 @Injectable()
 export class AppService {
-  getInitialUsers(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getInitialUsers(): Promise<object> {
+    try {
+      const apiUrl = this.configService.get<string>('INITIAL_DATA_API_URL');
+
+      const { data } = await firstValueFrom(
+        this.httpService.get<IInitialUsers>(apiUrl),
+      );
+
+      return JSON.parse(data.body);
+    } catch (error) {
+      console.log(error);
+      return {};
+    }
   }
 }
